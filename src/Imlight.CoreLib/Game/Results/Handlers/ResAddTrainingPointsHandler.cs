24a0dbe320794078e7f2f87846a5f8e24ad1f74d
Kevin Cazeneuve <78389784+KeviPiP@users.noreply.github.com>
2026-07-06 19:11:44 -0700

    quests and other triggers can now give training points!

---
+++ src/Imlight.CoreLib/Game/Results/Handlers/ResAddTrainingPointsHandler.cs
@@ -0,0 +1,45 @@
+using Akka.Actor;
+using Imcodec.MessageLayer.Generated;
+using Imcodec.ObjectProperty.TypeCache;
+using Imlight.Common;
+using Imlight.CoreLib.Shared.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imlight.CoreLib.Game.Results.Handlers;
+
+internal sealed class ResAddTrainingPointsHandler : BaseResultHandler<ResAddTrainingPoints> {
+    private const float QUERY_WIZARD_TIMEOUT_SECONDS = 5.0f;
+
+    public override bool Execute(IResultContext context) {
+        var queryWizardMsg = new CHARACTER_103_PROTOCOL.MSG_QUERYACTIVEWIZARD();
+        var queryTimeout = TimeSpan.FromSeconds(QUERY_WIZARD_TIMEOUT_SECONDS);
+        var playerRefActor = context.GetPlayerRef();
+        var queryResponse = playerRefActor
+            .Ask<CHARACTER_103_PROTOCOL.MSG_CHARACTER>(queryWizardMsg, queryTimeout).Result;
+        if (queryResponse == null) {
+            Logger.Error("Handler failed to retrieve character data within {0} seconds.",
+                Logger.Args(QUERY_WIZARD_TIMEOUT_SECONDS));
+
+            return false;
+        }
+
+        var wizard = queryResponse.Wizard;
+
+        var trainingPoints = wizard.MagicSchoolBehavior.TrainingPoints;
+        var newTrainingPoints = trainingPoints + 1;
+
+        wizard.UpdateTrainingPoints(newTrainingPoints);
+
+        var networkMessage = new WIZARD_12_PROTOCOL.MSG_UPDATETRAINING() {
+            TrainingPoints = newTrainingPoints
+        };
+
+        playerRefActor.Tell(networkMessage);
+
+        return true;
+    }
+}
